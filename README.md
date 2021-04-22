f2be4d8c1c790195452736076527072c871c5268
Forever <liuyongyuan@aliyun.com>
2021-04-22 15:08:22 +0800

    支持另一种裁剪方式

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # 图片裁剪插件
 
-支持正反向裁剪。
+支持两种裁剪方式。
 
-<img src="./readmeImg/picture.png" width="300" />
+<img src="./readmeImg/picture1.png" width="300" />
+<span> OR </sapn>
+<img src="./readmeImg/picture2.png" width="300" />
 <br/>
 
 ## 代码片段
 
 ```js
 var imageCropping = new ImageCropping({
-    canvas: document.querySelector('canvas'),  // 必填
-    img: myImg,                                // 必填
-    maxWidth: 800,                             // 可选，设置canvas宽度最大值
-//  maxHeight                                  // 可选，设置canvas高度最大值
+    canvas: document.querySelector('canvas'), // 必填
+    img: myImg, // 必填
+    unfixed: {
+        maxWidth: 800, // 可选，设置canvas宽度最大值
+        // maxHeight      可选，设置canvas高度最大值
+    },
+});
+
+// OR
+
+var imageCropping = new ImageCropping({
+    canvas: document.querySelector('canvas'), // 必填
+    img: myImg, // 必填
+    fixed: {
+        width: 500, // 必填，设置canvas宽度最大值
+        maximum: 8, // 可选，设置图片放大倍数默认6倍
+    },
 });
 ```
 
--- imageCropping.js
+++ imageCropping.js
@@ -1,21 +1,23 @@
 var ImageCropping = function (deploy) {
     this.canvas = deploy.canvas;
     this.img = deploy.img;
-    this.init(deploy.maxWidth, deploy.maxHeight);
+    this.fixedBool = this.toString.call(deploy.fixed) === '[object Object]';
+    this.unfixedBool = this.toString.call(deploy.unfixed) === '[object Object]';
+    if (this.fixedBool && this.unfixedBool) throw 'fixed and unfixed attributes cannot be written simultaneously';
+    if (!this.fixedBool && !this.unfixedBool) throw 'the fixed or unfixed property is not an Object';
+    this.init(deploy.fixed || deploy.unfixed);
 };
 ImageCropping.prototype = {
-    init: function (maxWidth, maxHeight) {
-        this.fitSize(maxWidth, maxHeight);
+    init: function (style) {
+        this.fitSize(style);
         this.ctx = this.canvas.getContext('2d');
-        this.isMousemove = false; // 捕获绘制区域的方法或光标 值是true时捕获方法，是false时捕获光标
+
         this.isMouseover = false;
         this.isThrottle = false; // 是否开启帧循环
-        this.cursorValue = { old: 'auto', new: 'auto' }; // 记录新旧光标值
-        this.zoomPointArr = [0, 0, 1, 0, 2, 0, 2, 1, 2, 2, 1, 2, 0, 2, 0, 1]; // 8个拖拽点坐标
-        this.resizeArr = ['nw-resize', 'n-resize', 'ne-resize', 'e-resize', 'se-resize', 's-resize', 'sw-resize', 'w-resize']; // 8个方向光标
-        this.coordinatePoint = []; // 记录上一个点的坐标位置
-        this.lineDash = [5, 5]; // 虚线间隔宽度
-        this.borderWidth = 2; // 边框宽度
+
+        if (this.fixedBool) this.fixed();
+
+        if (this.unfixedBool) this.unfixed();
 
         this.canvas.onmouseenter = function () {
             this.isMouseover = true;
@@ -29,9 +31,7 @@ ImageCropping.prototype = {
             this.isMousemove = true;
             this.offsetX = e.offsetX;
             this.offsetY = e.offsetY;
-            if (this.isThrottle) return this.draw();
-            this.isThrottle = true;
-            this.throttle(this.isThrottle);
+            this.draw();
         }.bind(this);
 
         this.canvas.onmousemove = function (e) {
@@ -40,29 +40,56 @@ ImageCropping.prototype = {
             if (this.isMousemove) {
                 this.isCapture && (this.isCapture = !this.isCapture);
             } else {
-                this.cursorValue.new = 'auto'; // 初始化坐标值
+                this.fixedBool && (this.cursorValue.new = 'move'); // 重置光标值
+                this.unfixedBool && (this.cursorValue.new = 'auto'); // 重置光标值
             }
         }.bind(this);
 
         this.canvas.onmouseleave = this.canvas.onmouseup = function (e) {
             // 这个判断暂时没用，这是为了将来可能兼容web端而做准备，填坑靠缘分
-            if (!this.isMouseover || e.type === 'mouseout') {
+            if (!this.isMouseover || e.type === 'mouseleave') {
                 this.isThrottle = false;
                 this.isMouseover && (this.isMouseover = false);
             }
+
             this.isMousemove = false;
             this.isCapture = false;
             this.method = void 0;
-
-            // 当高亮区域反转时宽高是负数，所以重新校正位置和大小
-            this.regionalCorrection();
+            // 当高亮区域反转时宽高是负数，需要重新校正位置和大小
+            this.unfixedBool && this.regionalCorrection();
         }.bind(this);
 
         this.draw();
     },
-    draw: function () {
+    unfixed: function () {
+        this.isMousemove = false; // 捕获绘制区域的方法或光标 值是true时捕获方法，是false时捕获光标
+        this.coordinatePoint = []; // 记录上一个点的坐标位置
+        this.cursorValue = { old: 'auto', new: 'auto' }; // 记录新旧光标值
+        this.zoomPointArr = [0, 0, 1, 0, 2, 0, 2, 1, 2, 2, 1, 2, 0, 2, 0, 1]; // 8个拖拽点坐标
+        this.resizeArr = ['nw-resize', 'n-resize', 'ne-resize', 'e-resize', 'se-resize', 's-resize', 'sw-resize', 'w-resize']; // 8个方向光标
+        this.lineDash = [5, 5]; // 虚线间隔宽度
+        this.borderWidth = 2; // 边框宽度
+        this.draw = this.unfixedDraw;
+    },
+    fixed: function () {
+        this.coordinatePoint = [0, 0];
+        this.offsetX = this.offsetY = 0;
+        this.cursorValue = { old: 'auto', new: 'move' }; // 记录新旧光标值
+        this.draw = this.fixedDraw;
+    },
+    fixedDraw: function () {
+        this.ctx.clearRect(-this.rangeMovement.left, -this.rangeMovement.top, this.imageWidth * this.ratio, this.imageHeight * this.ratio);
+        this.method && this.method(this.offsetX, this.offsetY); // 运行已捕获得到的方法
+
+        this.fixedRectangle();
+        this.regionZoom();
+
+        this.recordPoint(); // 记录上一个位置
+        if (!this.isMousemove) this.setCursor(); // 更新光标
+    },
+    unfixedDraw: function () {
         this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-        this.method && this.method.call(this, this.offsetX, this.offsetY); // 运行已捕获得到的方法
+        this.method && this.method(this.offsetX, this.offsetY); // 运行已捕获得到的方法
         this.boundaryDetection(); // canvas边界判定
         this.ctx.drawImage(this.img, 0, 0, this.canvasWidth, this.canvasHeight); // 绘制背景图片
         this.masking(); // 绘制蒙层
@@ -225,15 +252,99 @@ ImageCropping.prototype = {
             this.brightAreaRect[1][1] = Math.abs(this.brightAreaRect[1][1]);
         }
     },
+    fixedRectangle: function () {
+        this.ctx.drawImage(this.img, -this.rangeMovement.left, -this.rangeMovement.top, this.imageWidth * this.ratio, this.imageHeight * this.ratio); // 绘制背景图片
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.rect(0, 0, this.canvasWidth, this.canvasWidth);
+        if (this.ctx.isPointInPath(this.offsetX, this.offsetY)) {
+            this.isCapture &&
+                (this.method = function (x, y) {
+                    this.rangeMovement.left -= x - this.coordinatePoint[0];
+                    this.rangeMovement.top -= y - this.coordinatePoint[1];
+                    this.limitBoundary();
+                });
+
+            !this.isMousemove && (this.cursorValue.new = 'move');
+        }
+        this.ctx.closePath();
+        this.ctx.restore();
+    },
+    regionZoom: function () {
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.shadowBlur = 8;
+        this.ctx.shadowColor = 'white';
+        this.ctx.globalAlpha = 0.7;
+        this.ctx.strokeStyle = '#4166f5';
+        this.ctx.lineWidth = 3;
+        this.ctx.moveTo(this.amplificationRange[0], this.canvasWidth * 0.94);
+        this.ctx.lineTo(this.amplificationRange[1], this.canvasWidth * 0.94);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        this.ctx.restore();
+
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = '#4166f5';
+        this.ctx.fillStyle = 'white';
+        this.ctx.arc(this.amplificationRange[2], this.canvasWidth * 0.94, 5, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.stroke();
+        if (this.ctx.isPointInPath(this.offsetX, this.offsetY)) {
+            this.isCapture &&
+                (this.method = function (x) {
+                    var getProperty = this.imageWidth > this.imageHeight ? 'imageHeight' : 'imageWidth';
+                    this.rangeMovement.left = (this.rangeMovement.left + (this[getProperty] * (1 - this.ratio)) / 2) / this.ratio;
+                    this.rangeMovement.top = (this.rangeMovement.top + (this[getProperty] * (1 - this.ratio)) / 2) / this.ratio;
+
+                    this.amplificationRange[2] = x;
+                    if (this.amplificationRange[0] > x) this.amplificationRange[2] = this.amplificationRange[0];
+                    if (this.amplificationRange[1] < x) this.amplificationRange[2] = this.amplificationRange[1];
+
+                    this.ratio =
+                        this.maximum * ((this.amplificationRange[2] - this.amplificationRange[0]) / (this.amplificationRange[1] - this.amplificationRange[0])) +
+                        1;
+
+                    this.rangeMovement.left = this.rangeMovement.left * this.ratio - (this[getProperty] * (1 - this.ratio)) / 2;
+                    this.rangeMovement.top = this.rangeMovement.top * this.ratio - (this[getProperty] * (1 - this.ratio)) / 2;
+
+                    this.rangeMovement.right = this.imageWidth * this.ratio - this.canvasWidth;
+                    this.rangeMovement.bottom = this.imageHeight * this.ratio - this.canvasWidth;
+
+                    this.limitBoundary();
+                });
+            !this.isMousemove && (this.cursorValue.new = 'w-resize');
+        }
+        this.ctx.closePath();
+        this.ctx.restore();
+    },
+    limitBoundary: function () {
+        if (this.rangeMovement.left < 0) this.rangeMovement.left = 0;
+        if (this.rangeMovement.top < 0) this.rangeMovement.top = 0;
+
+        if (this.rangeMovement.left > this.rangeMovement.right) this.rangeMovement.left = this.rangeMovement.right;
+        if (this.rangeMovement.top > this.rangeMovement.bottom) this.rangeMovement.top = this.rangeMovement.bottom;
+    },
     clipping: function () {
         // 裁剪图片
-        !this.generateImgCtx && (this.generateImgCtx = document.createElement('canvas').getContext('2d'));
+        if (this.unfixedBool) {
+            !this.generateImgCtx && (this.generateImgCtx = document.createElement('canvas').getContext('2d'));
 
-        this.generateImgCtx.canvas.width = this.brightAreaRect[0][1];
-        this.generateImgCtx.canvas.height = this.brightAreaRect[1][1];
-        this.generateImgCtx.drawImage(this.img, -this.brightAreaRect[0][0], -this.brightAreaRect[1][0], this.canvasWidth, this.canvasHeight);
+            this.generateImgCtx.canvas.width = this.brightAreaRect[0][1];
+            this.generateImgCtx.canvas.height = this.brightAreaRect[1][1];
+            this.generateImgCtx.drawImage(this.img, -this.brightAreaRect[0][0], -this.brightAreaRect[1][0], this.canvasWidth, this.canvasHeight);
 
-        return this.generateImgCtx.canvas.toDataURL();
+            return this.generateImgCtx.canvas.toDataURL();
+        }
+
+        try {
+            this.ctx.clearRect(-this.rangeMovement.left, -this.rangeMovement.top, this.imageWidth * this.ratio, this.imageHeight * this.ratio);
+            this.fixedRectangle();
+            return this.canvas.toDataURL();
+        } finally {
+            this.regionZoom();
+        }
     },
     throttle: function () {
         this.draw();
@@ -245,42 +356,80 @@ ImageCropping.prototype = {
             this.cursorValue.old = this.cursorValue.new;
         }
     },
-    fitSize: function (maxWidth, maxHeight) {
-        var width = this.img.width,
-            height = this.img.height,
+    fitSize: function (style) {
+        var styleWidth,
+            styleHeight,
+            imgWidth = this.img.width,
+            imgHeight = this.img.height,
             rate;
 
-        if (maxWidth) {
-            rate = maxWidth / width;
-            width = maxWidth;
-            height = height * rate;
+        this.fixedBool && (styleWidth = style.width || this.canvasWidth);
+
+        if (this.unfixedBool) {
+            styleWidth = style.maxWidth;
+            styleHeight = style.maxHeight;
         }
 
-        if (maxHeight) {
+        if (styleWidth) {
+            rate = styleWidth / imgWidth;
+            imgWidth = styleWidth;
+            imgHeight *= rate;
+        }
+
+        if (this.fixedBool && imgHeight < styleWidth) {
+            rate = styleWidth / imgHeight;
+            imgWidth *= rate;
+            imgHeight = styleWidth;
+        }
+
+        if (this.unfixedBool && styleHeight) {
             if (!rate) {
-                rate = maxHeight / height;
-                height = maxHeight;
-                width = width * rate;
+                rate = styleHeight / imgHeight;
+                imgHeight = styleHeight;
+                imgWidth = imgWidth * rate;
             }
-            if (height > maxHeight) {
-                rate = maxHeight / height;
-                height = Math.ceil(rate * height);
+            if (imgHeight > styleHeight) {
+                rate = styleHeight / imgHeight;
+                imgHeight = Math.ceil(rate * imgHeight);
             }
-            maxWidth > maxHeight && (width = width * rate);
+            styleWidth > styleHeight && (imgWidth = imgWidth * rate);
         }
 
-        this.canvasWidth = this.canvas.width = width;
-        this.canvasHeight = this.canvas.height = height;
+        if (this.unfixedBool) {
+            this.canvasWidth = this.canvas.width = imgWidth;
+            this.canvasHeight = this.canvas.height = imgHeight;
 
-        // 初始化高亮区域的位置和大小
-        this.brightAreaRect = [
-            [(this.canvasWidth - this.canvasWidth / 3) / 2, this.canvasWidth / 3],
-            [(this.canvasHeight - this.canvasWidth / 3) / 2, this.canvasWidth / 3],
-        ];
+            // 初始化高亮区域的位置和大小
+            this.brightAreaRect = [
+                [(this.canvasWidth - this.canvasWidth / 3) / 2, this.canvasWidth / 3],
+                [(this.canvasHeight - this.canvasWidth / 3) / 2, this.canvasWidth / 3],
+            ];
+        }
+
+        if (this.fixedBool) {
+            this.canvasWidth = this.canvas.width = this.canvas.height = styleWidth;
+            this.imageWidth = imgWidth;
+            this.imageHeight = imgHeight;
+
+            this.rangeMovement = {
+                top: 0,
+                left: 0,
+                right: this.imageWidth - styleWidth,
+                bottom: this.imageHeight - styleWidth,
+            };
+            this.amplificationRange = [
+                (this.canvasWidth - this.canvasWidth * 0.9) / 2,
+                (this.canvasWidth - this.canvasWidth * 0.9) / 2 + this.canvasWidth * 0.9,
+            ];
+            this.amplificationRange[2] = this.amplificationRange[0];
+
+            this.maximum = (+style.maximum === +style.maximum ? +style.maximum : this.maximum || 6) - 1;
+            this.ratio = 1;
+        }
     },
-    replaceImg(img, maxWidth, maxHeight) {
+    replaceImg(img, style) {
         this.img = img;
-        this.fitSize(maxWidth, maxHeight);
+        this.fitSize(style || {});
         this.draw();
     },
 };
